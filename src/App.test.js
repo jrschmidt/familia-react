c08f610634018c501200f8ebd69db339a51e900c
jrschmidt <jrschmidt56@gmail.com>
2019-03-27 11:08:14 -0700

    add bare TreeDisplay3Gen component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TreeDisplay3Gen from './TreeDisplay3Gen';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders TreeDisplay3Gen component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TreeDisplay3Gen />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/TreeDisplay3Gen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import KidName from './KidName.js';
+
+class TreeDisplay3Gen extends Component {
+  render() {
+    return (
+      <div className="TreeDisplay3Gen">
+        <h2>TreeDisplay3Gen Component</h2>
+      </div>
+    )
+        }
+}
+
+export default TreeDisplay3Gen;
