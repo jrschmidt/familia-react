fe0eec88099894180e644139cb476c03c51d72d0
jrschmidt <jrschmidt56@gmail.com>
2019-04-02 13:04:35 -0700

    display data for persons from this.state.people in TreeDisplay3Gen component

--- src/TreeDisplay3Gen.js
+++ src/TreeDisplay3Gen.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import './TreeDisplay3Gen.css';
 // import KidName from './KidName.js';
 
 class TreeDisplay3Gen extends Component {
+
+  constructor () {
+    super ();
+
+    this.state = {
+
+      people : [
+        {
+          id: 'F-1',
+          surname: 'Anders',
+          firstname: 'Michael'
+        },
+
+        {
+          id: 'F-2',
+          surname: 'Bishop',
+          firstname: 'Ellen'
+        },
+
+        {
+          id: 'F-3',
+          surname: 'Anders',
+          firstname: 'Thomas'
+        },
+
+        {
+          id: 'F-4',
+          surname: 'Miller',
+          firstname: 'Joan'
+        },
+
+        {
+          id: 'F-5',
+          surname: 'Anders',
+          firstname: 'Dennis'
+        },
+
+        {
+          id: 'F-6',
+          surname: 'Anders',
+          firstname: 'Martha'
+        }
+      ]
+
+  };
+
+}
+
   render() {
     return (
       <div className="TreeDisplay3Gen">
         <h2>TreeDisplay3Gen Component</h2>
+        <p>Sample Text for TreeDisplay3Gen Component</p>
+        <ul>
+          {this.state.people.map( (person, key) => <li key={person.id}>{person.firstname} {person.surname}</li>)}
+        </ul>
       </div>
     )
         }
