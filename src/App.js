401c32a2531430fea6b494ceaf897aa462cb8d68
jrschmidt <jrschmidt56@gmail.com>
2019-04-04 08:30:18 -0700

    App component reads data from external dataService module

--- src/App.js
+++ src/App.js
@@ -1,65 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import getData from './dataService';
 import TreeDisplay3Gen from './TreeDisplay3Gen';
-
 
 class App extends Component {
 
-  constructor () {
-    super ();
-
-    this.state = {
-
-      people : [
-        {
-          id: 'F-1',
-          surname: 'Anders',
-          firstname: 'Michael'
-        },
-
-        {
-          id: 'F-2',
-          surname: 'Bishop',
-          firstname: 'Ellen'
-        },
-
-        {
-          id: 'F-3',
-          surname: 'Anders',
-          firstname: 'Thomas'
-        },
-
-        {
-          id: 'F-4',
-          surname: 'Miller',
-          firstname: 'Joan'
-        },
-
-        {
-          id: 'F-5',
-          surname: 'Anders',
-          firstname: 'Dennis'
-        },
-
-        {
-          id: 'F-6',
-          surname: 'Anders',
-          firstname: 'Martha'
-        }
-      ]
-
-    };
-
+  constructor (props) {
+    super(props);
+    let peopleData = getData().people;
+    this.state = { people: peopleData };
   }
 
-
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Familia-React</h1>
           <h2>React client for Familia genealogy API</h2>
-          <h5>familia-react v0.1.6</h5>
+          <h5>familia-react v0.1.7</h5>
         </header>
         <TreeDisplay3Gen people={this.state.people}/>
       </div>
@@ -67,6 +25,5 @@ class App extends Component {
   }
 
 }
-
 
 export default App;
---
+++ src/dataService.js
@@ -0,0 +1,47 @@
+export default function getData () {
+
+    return(
+
+      {
+      people : [
+        {
+          id: 'F-1',
+          surname: 'Anders',
+          firstname: 'Michael'
+        },
+
+        {
+          id: 'F-2',
+          surname: 'Bishop',
+          firstname: 'Ellen'
+        },
+
+        {
+          id: 'F-3',
+          surname: 'Anders',
+          firstname: 'Thomas'
+        },
+
+        {
+          id: 'F-4',
+          surname: 'Miller',
+          firstname: 'Joan'
+        },
+
+        {
+          id: 'F-5',
+          surname: 'Anders',
+          firstname: 'Dennis'
+        },
+
+        {
+          id: 'F-6',
+          surname: 'Anders',
+          firstname: 'Martha'
+        }
+      ]
+    }
+
+  );
+
+}
