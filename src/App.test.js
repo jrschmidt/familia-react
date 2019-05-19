a67ccbea83604dd118a3ad2879f3454ef2dd3ced
jrschmidt <jrschmidt56@gmail.com>
2019-05-19 16:58:39 -0700

    display a 'role' attribute for each person

--- src/App.test.js
+++ src/App.test.js
@@ -11,14 +11,14 @@ it('renders without crashing', () => {
 });
 
 it('renders TreeDisplay3Gen component', () => {
-  const people = [ {id: 'JS1', firstname: 'Joe', surname: 'Smith'} ];
+  const people = [ {firstname: 'Joe', surname: 'Smith', birthdate: '1969-07-20'} ];
   const div = document.createElement('div');
   ReactDOM.render(<TreeDisplay3Gen people={people} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders PersonMini component', () => {
-  const person = {firstname: 'Joe', surname: 'Smith'}
+  const person = {firstname: 'Joe', surname: 'Smith', birthdate: '1969-07-20'}
   const div = document.createElement('div');
   ReactDOM.render(<PersonMini person={person}/>, div);
   ReactDOM.unmountComponentAtNode(div);
--- src/PersonMini.js
+++ src/PersonMini.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class PersonMini extends Component {
+  // Temporary method to manually add a 'role' property to a person
+  manuallyAddPersonRole (person) {
+    let year = person.birthdate.slice(0,4);
+    let role;
+    switch (year) {
+      case '1957':
+        role = 'focus';
+      break;
+      case '1934':
+        role = 'father';
+      break;
+      case '1937':
+        role = 'mother';
+      break;
+      case '1955':
+        role = 'wife';
+      break;
+      default:
+      role = 'child';
+    }
+    return role;
+  }
+
   render () {
+    this.props.person.role = this.manuallyAddPersonRole(this.props.person);
     return (
       <div>
         <h4>PersonMini Component</h4>
         <p>{this.props.person.firstname} {this.props.person.surname}</p>
+        <h6>{this.props.person.role}</h6>
       </div>
     )
   }
