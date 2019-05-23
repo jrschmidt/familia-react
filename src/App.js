1c26c08289b873895f61ca9b2ee985af1ae61a7c
jrschmidt <jrschmidt56@gmail.com>
2019-05-23 12:15:32 -0700

    correct grid positioning within TreeDisplay3Gen component

--- src/App.js
+++ src/App.js
@@ -43,9 +43,7 @@ setDisplayProps (peopleData) {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Familia-React</h2>
-          <h3>React client for Familia genealogy API</h3>
-          <h5>familia-react v0.2.12</h5>
+          <h4>familia-react v0.2.12</h4>
         </header>
         <TreeDisplay3Gen people={this.state.people}/>
       </div>
