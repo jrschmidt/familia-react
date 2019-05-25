30068d9c278e9735fd2c67c96215bbc702f5457b
jrschmidt <jrschmidt56@gmail.com>
2019-05-25 06:52:50 -0700

    align the elements within the PersonMini component

--- src/PersonMini.js
+++ src/PersonMini.js
@@ -5,9 +5,10 @@ class PersonMini extends Component {
 
   render () {
     return (
-      <div className={this.props.person.role}>
-        <p>{this.props.person.firstname} {this.props.person.surname}</p>
+      <div className={'PersonMini ' + this.props.person.role}>
         <h6>{this.props.person.role}</h6>
+        <p>{this.props.person.firstname} {this.props.person.surname}</p>
+        <h5>b1917 d1997</h5>
       </div>
     )
   }
