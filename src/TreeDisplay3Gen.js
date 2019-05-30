b688261069945e6a52f75b41afcd0d73355080b5
jrschmidt <jrschmidt56@gmail.com>
2019-05-29 19:17:16 -0700

    TreeDisplay3Gen sets roles and display properties in state based on received props

--- src/TreeDisplay3Gen.js
+++ src/TreeDisplay3Gen.js
@@ -9,24 +9,43 @@ class TreeDisplay3Gen extends Component {
 
   constructor (props) {
     super(props);
-
     this.state = {
-      viewPersons: {
-        focus: this.props.people.find((person) => person._id === '5b0ef6074896a175634c115b'),
-        father: this.props.people.find((person) => person._id === '5b1048cba064560004bb6b6d'),
-        mother: this.props.people.find((person) => person._id === '5b0f02694896a175634c115c'),
-        wife: this.props.people.find((person) => person._id === '5b10442ea064560004bb6b6c'),
-        children: [ this.props.people.find((person) => person._id === '5b0f0cc44896a175634c115d') ]
-      }
+      readyToDisplay: 'no',
+      focus: {},
+      father: null,
+      mother: null,
+      husband: null,
+      wife: null,
+      children: []
+    }
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    return {
+      focus: props.people.find((person) => person._id === '5b0ef6074896a175634c115b'),
+      father: props.people.find((person) => person._id === '5b1048cba064560004bb6b6d'),
+      mother: props.people.find((person) => person._id === '5b0f02694896a175634c115c'),
+      wife: props.people.find((person) => person._id === '5b10442ea064560004bb6b6c'),
+      children: [ props.people.find((person) => person._id === '5b0f0cc44896a175634c115d') ]
     };
   }
 
   render() {
+    let list = this.state.focus ?
+    <div>
+    <p>focus {this.state.focus.firstname}</p>
+    <p>father {this.state.father.firstname}</p>
+    <p>mother {this.state.mother.firstname}</p>
+    <p>wife {this.state.wife.firstname}</p>
+    <p>child {this.state.children[0].firstname}</p>
+    </div>
+    :
+    'empty list';
+
     return (
       <div className="TreeDisplay3Gen">
         <TreeDisplayHeader />
-        <p>{Object.keys(this.state.viewPersons)}</p>
-        <p>{this.state.viewPersons['wife'].firstname}</p>
+        {list}
       </div>
     )
   }
