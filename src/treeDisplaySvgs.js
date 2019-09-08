9427aaffa1aa4cc0ee07007a8d13bf9c498499ff
jrschmidt <jrschmidt56@gmail.com>
2019-09-08 11:26:37 -0700

    redraw parent to child connector lines to proceed from a direct line between spouses

--- src/treeDisplaySvgs.js
+++ src/treeDisplaySvgs.js
@@ -97,20 +97,20 @@ const svgs = {
 
   // Use svgChildrenOfMale for male focus person.
   svgChildrenOfMale:
-    <svg className='connect-children-male' key='c-cm' width='560' height='120'>
-      <line x1='80' y1='40' x2='80' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='480' y1='0' x2='480' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='80' y1='80' x2='480' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='180' y1='80' x2='180' y2='120' stroke='#333333' strokeWidth='5' />
+    <svg className='connect-children-male' key='c-cm' width='240' height='160'>
+      <line x1='140' y1='40' x2='240' y2='40' stroke='#333333' strokeWidth='5' />
+      <line x1='190' y1='40' x2='190' y2='120' stroke='#333333' strokeWidth='5' />
+      <line x1='20' y1='120' x2='190' y2='120' stroke='#333333' strokeWidth='5' />
+      <line x1='20' y1='120' x2='20' y2='160' stroke='#333333' strokeWidth='5' />
     </svg>,
 
   // Use svgChildrenOfFemale for female focus person.
   svgChildrenOfFemale:
-    <svg className='connect-children-female' key='c-cf' width='560' height='120'>
-      <line x1='80' y1='0' x2='80' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='480' y1='40' x2=' 480' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='80' y1='80' x2='480' y2='80' stroke='#333333' strokeWidth='5' />
-      <line x1='380' y1='80' x2='380' y2='120' stroke='#333333' strokeWidth='5' />
+    <svg className='connect-children-female' key='c-cf' width='240' height='160'>
+      <line x1='0' y1='40' x2='100' y2='40' stroke='#333333' strokeWidth='5' />
+      <line x1='50' y1='40' x2=' 50' y2='120' stroke='#333333' strokeWidth='5' />
+      <line x1='50' y1='120' x2='220' y2='120' stroke='#333333' strokeWidth='5' />
+      <line x1='220' y1='120' x2='220' y2='160' stroke='#333333' strokeWidth='5' />
     </svg>
 };
 
