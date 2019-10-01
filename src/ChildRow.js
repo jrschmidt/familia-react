eb888adab9486c139c26ee925a9bdb6063b15ee2
jrschmidt <jrschmidt56@gmail.com>
2019-10-01 12:45:32 -0700

    move SVG definitions in ChildRow.js to treeDisplaySvgs.js

--- src/ChildRow.js
+++ src/ChildRow.js
@@ -2,78 +2,9 @@ import React, { Component } from 'react';
 import './ChildRow.css';
 import PersonMini from './PersonMini.js';
 import PersonMiniAdd from './PersonMiniAdd.js';
+import { getSiblingsSvg } from './treeDisplaySvgs.js';
 
 class ChildRow extends Component {
-
-  svgs = {
-
-    'single-1':
-      <svg className='connect' key='svg-single-1' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'single-2':
-      <svg className='connect' key='svg-single-2' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='500' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'single-3':
-      <svg className='connect' key='svg-single-3' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='200' y1='40' x2='600' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='200' y1='40' x2='200' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='400' y1='40' x2='400' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='600' y1='40' x2='600' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'final-1':
-      <svg className='connect' key='svg-final-1' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'final-2':
-      <svg className='connect' key='svg-final-2' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'final-3':
-      <svg className='connect' key='svg-final-3' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='500' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'final-4':
-      <svg className='connect' key='svg-final-4' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='700' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='700' y1='40' x2='700' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>,
-
-    'extend-4':
-      <svg className='connect' key='svg-extend-4' width='720' height='160'>
-        <line x1='400' y1='0' x2='400' y2='160' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='700' y2='40' stroke='#333333' strokeWidth='5' />
-        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
-        <line x1='700' y1='40' x2='700' y2='80' stroke='#333333' strokeWidth='5' />
-      </svg>
-
-  };
 
   generateRowTags() {
     if ( this.props.people.length === this.props.viewRoleTags.length ) {
@@ -100,7 +31,7 @@ class ChildRow extends Component {
             />
         );
       }
-      tags.push(this.svgs[this.props.rowType]);
+      tags.push(getSiblingsSvg(this.props.rowType));
       return tags;
     }
     else {
--- src/treeDisplaySvgs.js
+++ src/treeDisplaySvgs.js
@@ -75,6 +75,80 @@ const getChildrenSvg = (gender) => {
   }
 }
 
+const getSiblingsSvg = (rowType) => {
+  const svgs = {
+
+    'single-1':
+      <svg className='connect' key='svg-single-1' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'single-2':
+      <svg className='connect' key='svg-single-2' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='500' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'single-3':
+      <svg className='connect' key='svg-single-3' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='200' y1='40' x2='600' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='200' y1='40' x2='200' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='400' y1='40' x2='400' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='600' y1='40' x2='600' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'final-1':
+      <svg className='connect' key='svg-final-1' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'final-2':
+      <svg className='connect' key='svg-final-2' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'final-3':
+      <svg className='connect' key='svg-final-3' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='500' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'final-4':
+      <svg className='connect' key='svg-final-4' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='700' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='700' y1='40' x2='700' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>,
+
+    'extend-4':
+      <svg className='connect' key='svg-extend-4' width='720' height='160'>
+        <line x1='400' y1='0' x2='400' y2='160' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='700' y2='40' stroke='#333333' strokeWidth='5' />
+        <line x1='100' y1='40' x2='100' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='300' y1='40' x2='300' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='500' y1='40' x2='500' y2='80' stroke='#333333' strokeWidth='5' />
+        <line x1='700' y1='40' x2='700' y2='80' stroke='#333333' strokeWidth='5' />
+      </svg>
+
+  };
+  return svgs[rowType];
+
+}
+
 const getPlusSignSvg = () => {
   return (
     <svg className='plus-sign' width='31' height='31'>
@@ -97,4 +171,10 @@ const getPlusSignSvg = () => {
   );
 }
 
-export { getGrandparentSvg, getParentSvg, getChildrenSvg, getPlusSignSvg };
+export {
+  getGrandparentSvg,
+  getParentSvg,
+  getChildrenSvg,
+  getSiblingsSvg,
+  getPlusSignSvg
+};
